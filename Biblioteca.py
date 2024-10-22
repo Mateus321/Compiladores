273bcff27f99ec7092dd2dfe6f58107f44081310
Mateus Pereira Fernandes <mateuspereirafernandes@gmail.com>
2024-10-22 08:27:37 -0300

    Atualização da biblioteca de caracteres

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,4 +1,17 @@
 #BILIOTECA DE CONSTANTES
 
-OPERADOR = {'+': 'ADICAO', '-': 'SUBTRACAO', '*': 'MULTIPLICACAO', '/': 'DIVISAO', '=': 'ATRIBUICAO'}
-PALAVRAS_RESERVADAS = {'int': 'TIPO_INTEIRO', 'while': 'WHILE'}
+OPERADOR_ARITMETICO = {'+': 'ADICAO', '-': 'SUBTRACAO', '*': 'MULTIPLICACAO', '/': 'DIVISAO', '%': 'MODULO'}
+
+OPERADOR_LOGICO = {'||': 'OU_LOGICO', '&&': 'E_LOGICO', '!': 'NAO_LOGICO'}
+
+OPERADOR_RELACIONAL = {'==': 'IGUALDADE', '!=': 'DIFERENTE', '>': 'MAIOR_QUE', '>=': 'MAIOR_IGUAL_QUE', '<': 'MENOR_QUE', '<=': 'MENOR_IGUAL_QUE'}
+
+OPERADOR_ATRIBUICAO = {'=': 'ATRIBUIÇÃO', '=+': 'ATRIBUICAO_SOMA', '=-': 'ATRIBUICAO_SUBTRACAO', '*=': 'ATRIBUICAO_MULTIPLICACAO', '/=': 'ATRIBUICAO_DIVISAO', 
+                    '%=': 'ATRIBUICAO_MODULO'}
+
+PALAVRAS_RESERVADAS = {'int': 'TIPO_INTEIRO', 'float': 'TIPO_FLOAT', 'string': 'TIPO_STRING', 'for': 'TIPO_FOR', 'while': 'TIPO_WHILE', 'break': 'TIPO_BREAK', 
+                    'continue': 'TIPO_CONTINUE', 'if': 'TIPO_IF', 'else': 'TIPO_ELSE', 'return': 'TIPO_RETURN', 'system': 'TIPO_SYSTEM', 'out': 'TIPO_OUT', 
+                    'print': 'TIPO_PRINT', 'in': 'TIPO_IN', 'scan': 'TIPO_SCAN'}
+
+SIMBOLOS_ACEITOS = {';': 'PONTO_VIRGULA', ',': 'VIRGULA', '{': 'ABRE_CHAVE', '}': 'FECHA_CHAVE', '(': 'ABRE_PARENTESES', ')': 'FECHA_PARENTESES', '.': 'PONTO_FINAL'}
+
--- analisadorLexico.py
+++ analisadorLexico.py
@@ -1,16 +1,31 @@
 import sys # acessa os argumentos da linha de comando
 import os # verifica se o arquivo existe antes de abrir
-from Biblioteca import OPERADOR, PALAVRAS_RESERVADAS #chama a biblioteca que criamos
+from Biblioteca import OPERADOR_ARITMETICO, OPERADOR_LOGICO, OPERADOR_RELACIONAL, OPERADOR_ATRIBUICAO, SIMBOLOS_ACEITOS, PALAVRAS_RESERVADAS #chama a biblioteca que criamos
 
 
 # funcao que vai determinar o tipo do token
 def token_type(word):
-    if word in OPERADOR:
-        return OPERADOR[word] #retorna o tipo do operador
+    if word in OPERADOR_ARITMETICO:
+        return OPERADOR_ARITMETICO[word] #retorna o tipo do operador aritmetico
+    
+    elif word in OPERADOR_LOGICO:
+        return OPERADOR_LOGICO[word] #retorna o tipo do operador logico
+    
+    elif word in OPERADOR_RELACIONAL:
+        return OPERADOR_RELACIONAL[word] #retorna o tipo do operador relacional
+    
+    elif word in OPERADOR_ATRIBUICAO:
+        return OPERADOR_ATRIBUICAO[word] #retorna o tipo do operador de atribuicao
+    
+    elif word in SIMBOLOS_ACEITOS:
+        return SIMBOLOS_ACEITOS[word] #retorna o tipo do simbolo aceito
+    
     elif word in PALAVRAS_RESERVADAS:
         return PALAVRAS_RESERVADAS[word] #retorna o tipo da palavra reservada
+    
     elif word.isdigit():  # vai verificar se é um número
         return 'NUMERO'
+    
     else:
         return 'IDENTIFICADOR'  # se nao e operador e nem palavra reservada, entao e uma variavel
 
